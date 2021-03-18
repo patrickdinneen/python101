b1f6c373415bccea25e86f9f9707e44b7cec3a36
Patrick Dinneen <pdinneen@godaddy.com>
2021-03-18 09:15:24 +0200

    Requests, HTTP, APIs

--- tests/test_caesar.py
+++ tests/test_caesar.py
@@ -4,7 +4,7 @@ from cipher import caesar
 
 class CaesarTestCase(unittest.TestCase):
     def test_encrypt(self):
-        self.assertEqual(caesar.encrypt('the quick brown fox jumps over the lazy dog.'),
+        self.assertEqual(caesar.encrypt('the quick brown fox jumps over the lazy dog.', 23),
                          'qeb nrfzh yoltk clu grjmp lsbo qeb ixwv ald.')
 
     def test_get_offset_character(self):
